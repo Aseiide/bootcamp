31806830c954d2267ce45c25e35ff67df6534840
R-Tsukada <r.tsukada14@gmail.com>
2021-12-15 18:09:01 +0900

    コミット忘れがあったため、追加

--- db/schema.rb
+++ db/schema.rb
@@ -457,6 +457,7 @@ ActiveRecord::Schema.define(version: 2021_12_17_034500) do
     t.text "mentor_memo"
     t.string "discord_account"
     t.string "times_url"
+    t.text "after_graduation_hope"
     t.index ["course_id"], name: "index_users_on_course_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["github_id"], name: "index_users_on_github_id", unique: true
